28225edf37214e704b6cf8878a4b0f68a0ffaf17
Onyinye <onyinyeokanya@gmail.com>
2023-05-12 15:45:33 -0700

    libmy.a

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * @haystack: String to be searched
+ * @needle: substring to be searched for
+ * Return: pointer to the beginning of the located substring
+ *	if substring not found return NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}

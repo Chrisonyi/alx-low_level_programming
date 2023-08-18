d3029c87b75558857bcd1e138faef0acef1becad
Chrisonyi <onyinyeokanya@gmail.com>
2023-08-17 18:31:06 -0700

    7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+  insert_dnodeint_at_index(&head, 5, 100);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
+}
